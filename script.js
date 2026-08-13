4cf6c2557e7761b3af1e8310f418c5d66d3e31bb
Thapelo <tmatabane952@gmail.com>
2026-08-13 20:22:35 +0200

    completed the calculator project

---
+++ script.js
@@ -0,0 +1,34 @@
+let result = "";
+const buttonsGrid = document.querySelector(".buttons");
+const outputBox = document.querySelector(".output-box");
+    buttonsGrid.addEventListener('click', (e) => {
+        const buttonText = e.target.textContent;
+        if (buttonText === "C"){
+            result = "";
+            outputBox.textContent = 0;
+            
+        }
+        else if (buttonText === "="){
+            const resultNum = calculateString(result);
+            result = resultNum;
+            outputBox.textContent = resultNum;
+        }
+        else {
+            result = result + buttonText;
+            outputBox.textContent = result;
+        }
+
+    })
+
+function calculateString(expr) {
+
+  let cleanExpr = expr.replace(/×/g, '*').replace(/÷/g, '/');
+  
+  try {
+
+    return Function(`return ${cleanExpr}`)();
+  } catch (error) {
+    return "Error"; 
+  }
+}
+
